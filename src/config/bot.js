95f99683c942067888904a0163a062fa9a344a98
dimanovanov020-del <317643228+dimanovanov020-del@users.noreply.github.com>
2026-08-16 22:58:58 +0300

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -23,7 +23,7 @@ export const botConfig = {
     // 5 = Competing
     activities: [
       {
-        name: "Custom Statusss", // required by Discord API, not shown in the client
+        name: "Custom Status", // required by Discord API, not shown in the client
         state: "",     // this is what people actually see
         type: 3,               // Custom
       },
