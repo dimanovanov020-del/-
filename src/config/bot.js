128758de986cd6d4539423042d2e7f67275d23ba
dimanovanov020-del <dimanovanov020@gmail.com>
2026-08-16 21:45:25 +0300

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -24,8 +24,8 @@ export const botConfig = {
     activities: [
       {
         name: "Custom Status", // required by Discord API, not shown in the client
-        state: "Бот на тех работах",     // this is what people actually see
-        type: 3,               // Custom
+        state: "",     // this is what people actually see
+        type: 0,               // Custom
       },
     ],
   },
